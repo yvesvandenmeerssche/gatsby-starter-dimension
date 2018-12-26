75b1f2671176592e311d3a9b336490957164f813
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-26 11:53:19 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>LiveTheLifeTV</h1>
+                <p>"Does a guy with a demanding career living in a big city really want to stay in Kuta when he goes to Bali after dreaming about warm water, uncrowded waves and rice fields?  He might think he does, but what he really wants is a private LTL™ villa in Canggu with a maid and a cook with junkets to the next Ubud. Maybe even suggest Sri Lanka." A fully responsive site template designed by <a href="https://www.livethelife.tv">LTL</a>.</p>
             </div>
         </div>
         <nav>
