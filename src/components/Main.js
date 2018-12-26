192a6de86b1e9482a4783af35f1fe8761022b7c0
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-26 11:50:45 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import pic01 from '../images/pines.jpg'
+import pic02 from '../images/trees.jpg'
+import pic03 from '../images/wood.jpg'
 
 class Main extends React.Component {
   render() {
